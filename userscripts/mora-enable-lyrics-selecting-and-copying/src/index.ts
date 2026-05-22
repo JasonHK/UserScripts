0a43ded83ab280eb8333c21c5c4f5a7f0dc46fd5
Jason Kwok <4410086+JasonHK@users.noreply.github.com>
2026-05-22 07:08:41 +0000

    fix(mora-enable-lyrics-selecting-and-copying): more reliable device type checking

--- userscripts/mora-enable-lyrics-selecting-and-copying/src/index.ts
+++ userscripts/mora-enable-lyrics-selecting-and-copying/src/index.ts
@@ -13,7 +13,12 @@ const isLyricsPage = location.pathname.startsWith("/lyrics");
 
 window.addEventListener("load", () =>
 {
-    if (isLyricsPage || !isPC)
+    if (typeof isPC !== "boolean")
+    {
+        console.warn("Global variable isPC is either not exist or not a boolean (possibly website's internal changed), please contact the UserScript's maintainer for further investigation.");
+    }
+
+    if (isLyricsPage || ((typeof isPC === "boolean") ? !isPC : true))
     {
         console.info("Injecting inline style...");
         (document.head ?? document.documentElement).append(style);
@@ -26,10 +31,18 @@ EventTarget.prototype.addEventListener = new Proxy(EventTarget.prototype.addEven
     apply(target: addEventListener, that: EventTarget, args: Parameters<addEventListener>): ReturnType<addEventListener>
     {
         const type = args[0];
-        if (EVENT_TYPES.includes(type) && (that === document.body) && (isLyricsPage || !isPC))
+        if (EVENT_TYPES.includes(type) && (that === document.body))
         {
-            console.info("Defused \"%s\" event for %o", type, that);
-            return;
+            if (isLyricsPage || ((typeof isPC === "boolean") ? !isPC : true))
+            {
+                if (typeof isPC !== "boolean")
+                {
+                    console.warn("Global variable isPC is either not exist or not a boolean (possibly website's internal changed), please contact the UserScript's maintainer for further investigation.");
+                }
+
+                console.info("Defused \"%s\" event for %o", type, that);
+                return;
+            }
         }
 
         return Reflect.apply(target, that, args);
