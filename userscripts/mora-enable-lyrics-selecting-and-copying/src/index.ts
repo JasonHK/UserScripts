385b1d2098f7551d69392981a6f2127e45f21638
Jason Kwok <4410086+JasonHK@users.noreply.github.com>
2026-05-22 07:17:41 +0000

    chore(mora-enable-lyrics-selecting-and-copying): remove the additional console.warn

--- userscripts/mora-enable-lyrics-selecting-and-copying/src/index.ts
+++ userscripts/mora-enable-lyrics-selecting-and-copying/src/index.ts
@@ -31,18 +31,10 @@ EventTarget.prototype.addEventListener = new Proxy(EventTarget.prototype.addEven
     apply(target: addEventListener, that: EventTarget, args: Parameters<addEventListener>): ReturnType<addEventListener>
     {
         const type = args[0];
-        if (EVENT_TYPES.includes(type) && (that === document.body))
+        if (EVENT_TYPES.includes(type) && (that === document.body) && (isLyricsPage || ((typeof isPC === "boolean") ? !isPC : true)))
         {
-            if (isLyricsPage || ((typeof isPC === "boolean") ? !isPC : true))
-            {
-                if (typeof isPC !== "boolean")
-                {
-                    console.warn("Global variable isPC is either not exist or not a boolean (possibly website's internal changed), please contact the UserScript's maintainer for further investigation.");
-                }
-
-                console.info("Defused \"%s\" event for %o", type, that);
-                return;
-            }
+            console.info("Defused \"%s\" event for %o", type, that);
+            return;
         }
 
         return Reflect.apply(target, that, args);
